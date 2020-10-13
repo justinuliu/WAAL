caed92764d78e59c20d9ee47c471617b537d5909
Justin Liu <justinuliu@gmail.com>
2020-10-13 15:08:42 +1300

    Use configured proportion parameter

--- query_strategies/dis_entropy_mixture.py
+++ query_strategies/dis_entropy_mixture.py
@@ -233,7 +233,7 @@ class DisEntropyMixture:
             sys.stdout.write('\n')
 
         # Querying
-        query_num_part = int(query_num * 0.2)
+        query_num_part = int(query_num * self.args['repr_portion'])
         discriminator.eval()
         loader_te = DataLoader(self.test_handler(self.X[idx_ulb_train], self.Y[idx_ulb_train], transform=self.args['transform_te']),
                                shuffle=False, **self.args['loader_te_args'])
